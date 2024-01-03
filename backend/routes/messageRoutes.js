563ba81f320f1d9e70dbf20e9ab786da6f4d26e5
yash <officialyashdobriyal@gmail.com>
2024-01-03 21:01:54 +0530

    updated the message routes

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,8 +1,9 @@
 const express=require('express')
+const {sendMessage, allMessage}= require('../controller/messageController')
 const { protect } = require('../middleware/authMiddleware')
 const router=express.Router()
-// router.route('/').post(protect, sendMessage)
-// router.route('/:chatId').post(protect, allMessage)
+router.route('/').post(protect, sendMessage)
+router.route('/:chatId').post(protect, allMessage)
 
 
 module.exports=router;
