9bc5adf35cd69fb57d52e16ea0861a65e635c41d
YashDobriyal <110299809+SIGEGV@users.noreply.github.com>
2024-03-18 17:48:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,7 @@ app.use(express.json());   //The express.json() function is a middleware
 
 // --------------------------routes------------------------------- 
 
-app.use(cors({
-  origin: 'https://weeby-chat.netlify.app', // Replace with your Netlify frontend URL
-  credentials: true // If your backend sends cookies, set credentials to true
-}));
+app.use(cors())
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/messages", messageRoutes);
